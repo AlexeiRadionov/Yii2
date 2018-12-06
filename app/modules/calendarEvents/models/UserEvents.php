9bb91fd55f2aacd4a2f9bf8c96c2676cc14d6198
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-07 03:45:41 +0600

    создал модель с классом события пользователя

---
+++ app/modules/calendarEvents/models/UserEvents.php
@@ -0,0 +1,17 @@
+<?php
+	namespace app\modules\calendarEvents\models;
+	use yii\base\Model;
+
+	class UserEvents extends Model {
+	    public $title;
+	    public $startDay;
+	    public $endDay;
+	    public $idAuthor;
+	    public $description;
+
+	    public function __construct($config=[], $attributes=[]) {
+	    	parent::__construct($config);
+	    	$this -> setAttributes($attributes, false);
+	    }
+	}
+?>
