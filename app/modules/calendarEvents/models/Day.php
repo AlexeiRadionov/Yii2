a8b23aed25a4a05673bf558ab202337eb0544ab3
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-09 16:07:09 +0600

    добавил вывод событий на день

--- app/modules/calendarEvents/models/Day.php
+++ app/modules/calendarEvents/models/Day.php
@@ -3,14 +3,38 @@
 	use app\modules\calendarEvents\models\UserEvents;
 
 	class Day extends UserEvents {
-		public $typeDay;
-		public $event;
+		public $selectedDate;
+		public $eventsDay = [];
+		public $selectedDay;
 
-		public function getCurrentDay() {
-			if (date('N') >= 6) {
-				$this -> typeDay = 'выходной день';
+		public function __construct($config=[]) {
+	    	parent::__construct($config);
+	    }
+
+		public function isWork() {
+			$w = date_create($this -> selectedDate) -> format('w');
+			
+			return !in_array($w, [0, 6]);
+		}
+
+		public function isWeekend() {
+			return !$this -> isWork();
+		}
+
+		public function getselectedDate($selectedDate) {
+			$this -> selectedDate = $selectedDate;
+			if ($this -> isWeekend()) {
+				$this -> selectedDay = 'выходной день';
 			} else {
-				$this -> typeDay = 'рабочий день';
+				$this -> selectedDay = 'рабочий день';
+			}
+		}
+
+		public function getEventsDay($eventsUser) {
+			foreach ($eventsUser as $key => $value) {
+				if ($value['startDay'] <= $this -> selectedDate && $this -> selectedDate <= $value['endDay']) {
+					array_push($this -> eventsDay, $value);
+				}
 			}
 		}
 	}
