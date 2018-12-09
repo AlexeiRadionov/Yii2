1e295fbe307f9912ac8979e42143c2f69589a1d7
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-09 16:08:46 +0600

    добавил ссылку на страницу с выбранным днём

--- app/modules/calendarEvents/views/default/index.php
+++ app/modules/calendarEvents/views/default/index.php
@@ -1,7 +1,7 @@
 <div>
     <h1>My events</h1>
 
-    <?php foreach ($events as $event): ?>
+    <?php foreach ($eventsUser as $event): ?>
         <div>
             <h3><?php echo $event -> title ?></h3>
             <p><?php echo $event -> startDay . ' - ' . $event -> endDay ?></p>
@@ -9,4 +9,6 @@
         </div>
         <hr>
     <?php endforeach; ?>
+
+    <a href="/calendarEvents/default/day">Events selected day</a>
 </div>
