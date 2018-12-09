3ed8de81bc0e09025ff18ec669c91c11e1c2ee4e
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-09 16:07:59 +0600

    доработал метод actionDay

--- app/modules/calendarEvents/controllers/DefaultController.php
+++ app/modules/calendarEvents/controllers/DefaultController.php
@@ -8,43 +8,53 @@
 	 * Default controller for the `calendarEvents` module
 	 */
 	class DefaultController extends Controller {
+		public $eventsUser;
 	    /**
 	     * Renders the index view for the module
 	     * @return string
 	     */
 	    public function actionIndex() {
-	    	$events = [
+	    	$this -> getEventsUser();
+
+	        return $this->render('index', ['eventsUser' => $this -> eventsUser]);
+	    }
+
+	    public function getEventsUser() {
+	    	$eventsUser = [
 	    		new UserEvents([], [
 	    			'title' => 'Event 1',
 	    			'description' => 'Lorem ipsum dolor.',
-	    			'startDay' => date('Y.m.d H:i:s'),
-	    			'endDay' => date('Y.m.d H:i:s', time() + 3 * 24 * 3600)
+	    			'startDay' => '2018-11-30',
+	    			'endDay' => '2018-12-10'
 	    		]),
 	    		new UserEvents([], [
 	    			'title' => 'Event 2',
 	    			'description' => 'Lorem ipsum dolor.',
-	    			'startDay' => date('Y.m.d H:i:s'),
-	    			'endDay' => date('Y.m.d H:i:s', time() + 3 * 24 * 3600)
+	    			'startDay' => '2018-12-07',
+	    			'endDay' => '2018-12-12'
 	    		]),
 	    		new UserEvents([], [
 	    			'title' => 'Event 3',
 	    			'description' => 'Lorem ipsum dolor.',
-	    			'startDay' => date('Y.m.d H:i:s'),
-	    			'endDay' => date('Y.m.d H:i:s', time() + 3 * 24 * 3600)
+	    			'startDay' => '2018-12-23',
+	    			'endDay' => '2018-12-31'
 	    		])
 	    	];
-	        return $this->render('index', ['events' => $events]);
+
+	    	$this -> eventsUser = $eventsUser;
 	    }
 
 	    public function actionDay() {
+	    	$selectedDate = date('Y-m-d');
 	    	$day = new Day;
-	    	$day -> getCurrentDay();
-	    	$event = 'Event 4';
+	    	$day -> getSelectedDate($selectedDate);
+	    	$this -> getEventsUser();
+	    	$day -> getEventsDay($this -> eventsUser);
 
 	    	return $this->render('viewDay', 
 	    		[
-	    		'day' => $day -> typeDay,
-	    		'event' => $event
+	    		'day' => $day -> selectedDay,
+	    		'eventsDay' => $day -> eventsDay
 	    		]);
 	    }
 	}
