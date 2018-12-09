cebfb3ab57ae1cd3a1c26fa2cfe9822a2a88040c
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-09 16:09:53 +0600

    добавил вывод событий на день

--- app/modules/calendarEvents/views/default/viewDay.php
+++ app/modules/calendarEvents/views/default/viewDay.php
@@ -1,2 +1,10 @@
-<p>Сегодня: <?php echo $day ?></p>
-<p>События на сегодня: <?php echo $event ?></p>
+<p>Сегодня: <?php echo date('Y-m-d') . " $day"?></p>
+<p>События на сегодня:</p>
+<?php foreach ($eventsDay as $event): ?>
+	<div>
+        <h3><?php echo $event -> title ?></h3>
+        <p><?php echo $event -> startDay . ' - ' . $event -> endDay ?></p>
+        <p><?php echo $event -> description ?></p>
+    </div>
+    <hr>
+<?php endforeach; ?>
