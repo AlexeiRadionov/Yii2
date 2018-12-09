9b25124273f08b066bec6cf7eb2ca425d6255399
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-10 01:03:44 +0600

    доработал представление день

--- app/modules/calendarEvents/views/default/viewDay.php
+++ app/modules/calendarEvents/views/default/viewDay.php
@@ -1,8 +1,11 @@
-<p>Сегодня: <?php echo date('Y-m-d') . " $day"?></p>
-<p>События на сегодня:</p>
+<a href="/calendarEvents">List events</a>
+<span>/</span>
+<span>Events to day</span>
+<h4>Today: <?php echo date('d-m-Y') . " $day"?></h4>
+<p>Events today:</p>
 <?php foreach ($eventsDay as $event): ?>
 	<div>
-        <h3><?php echo $event -> title ?></h3>
+        <h3><a href="/calendarEvents/default/form" title="Edit event"><?php echo $event -> title ?></a></h3>
         <p><?php echo $event -> startDay . ' - ' . $event -> endDay ?></p>
         <p><?php echo $event -> description ?></p>
     </div>
