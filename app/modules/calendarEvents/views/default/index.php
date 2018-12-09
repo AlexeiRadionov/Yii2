c5f37f066f281e4c1226d31da51557a7978e12af
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-10 01:01:31 +0600

    добавил навигацию по приложению

--- app/modules/calendarEvents/views/default/index.php
+++ app/modules/calendarEvents/views/default/index.php
@@ -1,14 +1,17 @@
+<span>List events</span>
+<span>/</span>
+<a href="/calendarEvents/default/day">Events today</a>
+<span>/</span>
+<a href="/calendarEvents/default/form">Add event</a>
 <div>
     <h1>My events</h1>
 
     <?php foreach ($eventsUser as $event): ?>
         <div>
-            <h3><?php echo $event -> title ?></h3>
+            <h3><a href="/calendarEvents/default/form" title="Edit event"><?php echo $event -> title ?></a></h3>
             <p><?php echo $event -> startDay . ' - ' . $event -> endDay ?></p>
             <p><?php echo $event -> description ?></p>
         </div>
         <hr>
     <?php endforeach; ?>
-
-    <a href="/calendarEvents/default/day">Events selected day</a>
 </div>
