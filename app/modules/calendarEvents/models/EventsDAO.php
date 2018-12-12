9a3085c4000a8fe4911da69eb15d2a61521fd448
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-13 00:22:51 +0600

    создал модуль работы с базой данных

---
+++ app/modules/calendarEvents/models/EventsDAO.php
@@ -0,0 +1,47 @@
+<?php
+	namespace app\modules\calendarEvents\models;
+	
+	use yii\base\Model;
+	use Yii;
+
+	class EventsDAO extends Model {
+		
+		public function createEvent($event, $id_user) {
+			$db = Yii::$app -> db;
+
+			$result = $db -> createCommand() -> insert('events', [
+				'title' => $event -> title,
+				'startDay' => $event -> startDay,
+				'endDay' => $event -> endDay,
+				'id_user' => $id_user,
+				'description' => $event -> description,
+				'isBlock' => $event -> isBlock
+			]) -> execute();
+
+			return $result > 0;
+		}
+
+		public function getEvents() {
+			$db = Yii::$app -> db;
+
+			$result = [];
+			$result = $db -> createCommand('SELECT * FROM events') -> queryAll();
+			
+			return $result;
+		}
+
+		public function editEvent($event, $id_events) {
+			$db = Yii::$app -> db;
+
+			$result = Yii::$app->db->createCommand() -> update('events', [
+				'title' => $event -> title,
+				'startDay' => $event -> startDay,
+				'endDay' => $event -> endDay,
+				'description' => $event -> description,
+				'isBlock' => $event -> isBlock
+			], "id_events = $id_events") -> execute();
+			
+			return $result > 0;
+		}
+	}
+?>
