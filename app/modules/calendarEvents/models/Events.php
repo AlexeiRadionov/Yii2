48ed79f74b61cea16f0fc0a4c8c671393340015f
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-19 17:28:08 +0600

    добавил в валидацию проверку формата даты из конфига

--- app/modules/calendarEvents/models/Events.php
+++ app/modules/calendarEvents/models/Events.php
@@ -3,13 +3,14 @@
 	
 	use app\models\User;
 	use yii\db\ActiveRecord;
+	use Yii;
 
 	class Events extends ActiveRecord {
 
 	    public function rules() {
 	        return [
-	            [['title','description'], 'required'],
-	            [['startDay', 'endDay'], 'date', 'format' => 'yyyy-mm-dd'],
+	            [['title','description', 'startDay'], 'required'],
+	            [['startDay', 'endDay'], 'date', 'format' => Yii::$app->params['dateFormatModel']],
 	            ['endDay', 'compare', 'compareAttribute' => 'startDay', 'operator' => '>=', 'enableClientValidation' => false],
 	            ['endDay', 'default', 'value' => function($model) {
 	            		return $model -> startDay;
