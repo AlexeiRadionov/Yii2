3768c3a23d68e69d950df1e39a600ba4c8937f46
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-23 04:50:43 +0600

    добавил в форму поле для ввода пароля

--- app/modules/calendarEvents/views/user/_form.php
+++ app/modules/calendarEvents/views/user/_form.php
@@ -14,6 +14,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'password')->textInput(['placeholder' => 'Your current password']) ?>
+
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
