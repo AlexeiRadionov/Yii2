bf1b82560d653dae7ec6721e458be2340f4de1ef
AlexeiRadionov <radionov-aleksei@mail.ru>
2018-12-28 02:40:27 +0600

    удалил

--- app/modules/calendarEvents/views/default/submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$this->params['breadcrumbs'][] = ['url' => '/calendarEvents', 'label' => 'List events'];
-    $this->params['breadcrumbs'][] = ['url' => '/calendarEvents/default/day', 'label' => 'Events today'];
-?>
-
-<p>Event successfully saved</p>
