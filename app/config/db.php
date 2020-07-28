d2b11f251b51a1cea83ae06c80650b8f6352b0db
RadionovAlexey <radionov-aleksei@mail.ru>
2020-07-28 05:29:41 +0300

    Revert "change password db"
    This reverts commit 7f79e2eaa679589ed1f96b85ad9752666c14fe29.

--- app/config/db.php
+++ app/config/db.php
@@ -7,7 +7,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=calendar',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
